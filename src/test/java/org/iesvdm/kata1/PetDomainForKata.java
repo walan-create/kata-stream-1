688c0c4b58dc09e404edb95725194b0b62c33386
toteabe <toteabe@yahoo.es>
2023-09-05 18:21:33 +0200

    continua

--- src/test/java/org/iesvdm/kata1/PetDomainForKata.java
+++ src/test/java/org/iesvdm/kata1/PetDomainForKata.java
@@ -1,17 +1,7 @@
-/*
- * Copyright (c) 2022 Goldman Sachs.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v. 1.0 which accompany this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- */
 
 package org.iesvdm.kata1;
 
-import org.eclipse.collections.api.list.MutableList;
-import org.eclipse.collections.impl.factory.Lists;
+
 import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Arrays;
@@ -42,6 +32,6 @@ public abstract class PetDomainForKata
 
     public Person getPersonNamed(String fullName)
     {
-        return this.people.stream().filter(p -> p.getFullName().equals(fullName)).findAny().;
+        return new Person("un", "nombre"); //TODO
     }
 }
